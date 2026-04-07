ac5d3b8c0a7d7e4f2a2352a96dd5cb439f346cb9
bizGenie <bizgenieyg@gmail.com>
2026-04-07 11:30:53 +0300

    fix: add currency ILS to cancelGreenInvoiceDocument request body

--- integrations/greeninvoice.js
+++ integrations/greeninvoice.js
@@ -104,7 +104,8 @@ export async function cancelGreenInvoiceDocument(documentId) {
       type: 410,
       linkedDocumentId: documentId,
       lang: 'he',
-      signed: true
+      signed: true,
+      currency: 'ILS'
     })
   })
 
